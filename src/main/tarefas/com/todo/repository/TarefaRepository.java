8b73df02a20f663c15632261c419005e8b132853
cm-elaine <elainecalazans14@gmail.com>
2024-12-04 19:49:20 -0300

    Removendo arquivos antigos

--- src/main/tarefas/com/todo/repository/TarefaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.todo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.todo.model.Tarefa;
-
-public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
-}
